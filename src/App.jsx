f50493f8ac28896d2d1a05da8c58fd51e26af855
antoine leloup <al@MacBook-Air.local>
2024-03-25 14:50:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://site--deliveroo-backend--dx5qb8qmqmcb.code.run/"
+    );
+    console.log(response.data);
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <h2>{data.restaurant.name}</h2>
+  );
+}
+
+export default App;
